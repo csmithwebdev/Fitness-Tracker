6fc064d59a83fb8378ef33714afb4eb5ba924a78
Casey <casey@csmithwebdev.com>
2022-01-17 21:49:29 -0500

    new additions

--- src/App.js
+++ src/App.js
@@ -7,12 +7,22 @@ import './styles.css';
 const App = () => {
 const [value, onChange] = useState(new Date());
 
+const userDetails = {
+	name: 'Casey',
+	weight: null,
+	goalWeight: null,
+	successDays: null,
+	failedDays: null,
+	daysLeft: null
+};
+
+
 return (
 	<div className="ui container">
 		<div className="ui grid">
 			<div id="panel" className="four wide column">
-				<h3>Hello, Casey</h3>
-				<p>Current Weight: 185</p>
+				<h3>Hello, {userDetails.name}</h3>
+				<p>Current Weight: 185 <a className="update_button" href="#">update</a></p>
 				<p>Goal Weight: 160</p>
 				<p>Successful days: 1</p>
 				<p>Failed days: 0</p>
@@ -20,7 +30,7 @@ return (
 			</div>
 			<div className="twelve wide column">
 				<Calendar
-					onChange={onChange}
+					onChange={(value, onChange) => alert('Clicked day: ', value)}
 					value={value}
 				/>
 			</div>
